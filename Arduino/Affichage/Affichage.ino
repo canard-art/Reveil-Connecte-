4b6595dd97df426a09a9fc31118adf9f3c9f0163
canard-art <phenprapha2005@gmail.com>
2025-06-02 23:25:43 +0200

    Update Affichage.ino

--- Arduino/Affichage/Affichage.ino
+++ Arduino/Affichage/Affichage.ino
@@ -1,87 +1,79 @@
 #include <Wire.h>
-#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <Adafruit_SHTC3.h>  
+#include <Adafruit_SHTC3.h>
 
-// Dimensions de l'écran OLED
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_SHTC3 shtc3 = Adafruit_SHTC3();  
 
-// Capteur SHTC3
-Adafruit_SHTC3 shtc3 = Adafruit_SHTC3();
-
-// Variables capteurs
-float temperature = 0.0;
-float humidite = 0.0;
-int luminosite = 82;  // Tu peux le connecter à un capteur LDR si tu veux
-String heure = "18:00"; // Optionnel, à remplacer par un RTC plus tard
+String heure = "";
 
 void setup() {
   Serial.begin(9600);
-  Wire.begin();
-
-  // Initialisation écran OLED
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("Échec de l'écran OLED"));
-    while (true);
-  }
-
-  // Initialisation capteur SHTC3
-  if (!shtc3.begin()) {
-    Serial.println("Erreur capteur SHTC3");
-    display.clearDisplay();
-    display.setCursor(0, 0);
-    display.println("Erreur SHTC3");
-    display.display();
-    while (true);
+  
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    while(true); 
   }
 
   display.clearDisplay();
-  display.setTextColor(SSD1306_WHITE);
   display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
-  display.println("Initialisation...");
+  display.print("Initialisation...");
+  display.display();
+
+  if (!shtc3.begin()) {
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.print("SHTC3 erreur!");
+    display.display();
+    while (1);
+  }
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print("Pret !");
   display.display();
   delay(1000);
 }
 
 void loop() {
-  // Lecture des données capteur
-  sensors_event_t humidityEvent, tempEvent;
-  shtc3.getEvent(&humidityEvent, &tempEvent);
-  temperature = tempEvent.temperature;
-  humidite = humidityEvent.relative_humidity;
+  sensors_event_t humidity, temp;
+  shtc3.getEvent(&humidity, &temp);  
+
+  if (Serial.available() > 0) {
+    heure = Serial.readStringUntil('\n');  
+  }
 
   display.clearDisplay();
 
-  // Affichage heure
-  display.setTextSize(2);
-  display.setCursor(0, 0);
-  display.print(heure);
   display.setTextSize(1);
-  display.setCursor(100, 5);
-  display.print("☀");  // Icône soleil
+  display.setCursor(0, 0);
+  display.print("Heure:");
+  display.setCursor(40, 0);
+  display.print(heure);
 
-  // Température
-  display.setCursor(0, 25);
-  display.print("Temp : ");
-  display.print(temperature, 1);  // 1 décimale
-  display.println(" C");
+  display.setCursor(0, 20);
+  display.print("Temp: ");
+  display.print(temp.temperature, 1);
+  display.print(" C");
 
-  // Humidité
-  display.setCursor(0, 38);    
-  display.print("Hum  : ");
-  display.print(humidite, 1);
-  display.println(" %");
-
-  // Luminosité (simulée ici)
-  display.setCursor(0, 51);
-  display.print("Lumi : ");
-  display.print(luminosite);
-  display.println(" %");
+  display.setCursor(0, 35);
+  display.print("Hum:  ");
+  display.print(humidity.relative_humidity, 1);
+  display.print(" %");
 
   display.display();
-  delay(2000);
+
+  Serial.print("Heure:");
+  Serial.print(heure);
+  Serial.print(" Temp:");
+  Serial.print(temp.temperature, 1);
+  Serial.print(" Hum:");
+  Serial.println(humidity.relative_humidity, 1);
+
+  delay(1000);  
 }
